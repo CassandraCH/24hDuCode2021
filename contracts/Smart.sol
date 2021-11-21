66c5fd2e2f0380f156fc6f282434609868d38ad5
Yann Navigateur Soreau <yann.soreau@infotel.com>
2021-11-21 03:40:26 +0100

    ajout plusieurs stratégies

---
+++ contracts/Smart.sol
@@ -0,0 +1,21 @@
+pragma solidity >=0.4.22 <0.6.0;
+import "./ActionStrategy.sol";
+import "./AvatarInterface.sol";
+
+contract Smart is ActionStrategy {
+
+    /**
+     * Choisi l'action d'un avatar lors d'un tour d'un combat.
+     *
+     * @param previousAdvAction l'action précédente de l'avatar adverse
+     * @param avatar adresse de l'avatar courant
+     * @param damages blessures subites depuis le debut du combats
+     * @param avatarAdv adresse de l'avatar adverse
+     * @param damagesAdv blessures subites depuis le debut du combats
+     * @return l'action de l'avatar pour le tour courant
+     */
+    function chooseNextAction(Action previousAdvAction, address avatar, int damages, address avatarAdv, int damagesAdv) external view returns(Action action){
+        return Action.DODGE;
+    }
+    
+}
